8c58821601eb399e18b5784f535cca8dfed628da
Fayez Ahmed <fayez.ahmed@hotmail.co.uk>
2021-06-29 14:41:29 +0100

    Update simplePipeline.groovy

--- vars/simplePipeline.groovy
+++ vars/simplePipeline.groovy
@@ -1,60 +1,62 @@
-pipeline {
-    agent any
-    
-    environment {
-        RELEASE='0.2'
-    }
+def call() {
+    pipeline {
+        agent any
 
-    stages {
-        stage('Git Checkout') {
-            steps {
-                // Get some code from a GitHub repository
-               git branch: 'main', url: 'https://github.com/Fayez74/jgsu-spring-petclinic.git'
-            }
+        environment {
+            RELEASE='0.2'
         }
-        stage('Build') {
-            input {
-                message 'Build Now?'
-                ok 'Approve'
-            }
-            steps {
-                // Run Maven on a Unix agent.
-                slackSend channel: 'general', message: 'OHHH SHIIIIT, job started'
-                sh "./mvnw clean package"
-            }
-            
 
-            post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                success {
-                    junit 'target/surefire-reports/*.xml'
-                    archiveArtifacts 'target/*.jar'
-                    slackSend channel: 'general', 
-                    message: "${env.RELEASE} succeded"
-                } 
-               
-            }
-        }
-        stage('Final Test') {
-            steps {
-                echo "Test release with random value"
-                script {
-                    if ("${RELEASE}" < 0.5 ) {
-                        writeFile file: 'test-results.txt', text:'Release needs updating'
-                        sh 'cat test-results.txt'
-                    }else {
-                        writeFile file: 'test-results.txt', text:'Release up to date'
-                        sh 'cat test-results.txt'
-                    }
-                    showDate()
+        stages {
+            stage('Git Checkout') {
+                steps {
+                    // Get some code from a GitHub repository
+                   git branch: 'main', url: 'https://github.com/Fayez74/jgsu-spring-petclinic.git'
                 }
-                
+            }
+            stage('Build') {
+                input {
+                    message 'Build Now?'
+                    ok 'Approve'
+                }
+                steps {
+                    // Run Maven on a Unix agent.
+                    slackSend channel: 'general', message: 'OHHH SHIIIIT, job started'
+                    sh "./mvnw clean package"
+                }
+
+
+                post {
+                    // If Maven was able to run the tests, even if some of the test
+                    // failed, record the test results and archive the jar file.
+                    success {
+                        junit 'target/surefire-reports/*.xml'
+                        archiveArtifacts 'target/*.jar'
+                        slackSend channel: 'general', 
+                        message: "${env.RELEASE} succeded"
+                    } 
+
+                }
+            }
+            stage('Final Test') {
+                steps {
+                    echo "Test release with random value"
+                    script {
+                        if ("${RELEASE}" < 0.5 ) {
+                            writeFile file: 'test-results.txt', text:'Release needs updating'
+                            sh 'cat test-results.txt'
+                        }else {
+                            writeFile file: 'test-results.txt', text:'Release up to date'
+                            sh 'cat test-results.txt'
+                        }
+                        showDate()
+                    }
+
+                }
             }
         }
     }
-}
 
-String showDate() {
-    sh 'date'
+    String showDate() {
+        sh 'date'
+    }
 }
